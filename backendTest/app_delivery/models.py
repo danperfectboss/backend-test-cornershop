9913821a24e11a4ae4ca73f245896781c0d1e9a6
Daniel Perfecto Mendoza <dapeme.boss@gmail.com>
2021-04-26 18:03:29 -0500

    Se generaron y modificaron los modelos para la estructura y almacenamiento de los datos en la base

--- backendTest/app_delivery/models.py
+++ backendTest/app_delivery/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 class Recipe(models.Model):
     id_recipe = models.AutoField(primary_key=True)
-    description = models.CharField(max_length=700)
+    description = models.CharField(max_length=1000)
     date = models.DateField(auto_now=False, auto_now_add=False)
 
 class Employees(models.Model):
     id_employee = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=400)
     id_recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+class CustomRecipes(models.Model):
+    id_customRecipe = models.BigAutoField(primary_key=True)
+    id_recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    id_employe = models.ForeignKey(Employees, on_delete=models.CASCADE)
+    commentary  = models.CharField(max_length=500)
