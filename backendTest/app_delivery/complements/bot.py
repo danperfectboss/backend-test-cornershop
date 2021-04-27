4d043c2c5306475b159119f66b9178307a7eb1b6
Daniel Perfecto Mendoza <dapeme.boss@gmail.com>
2021-04-27 17:58:10 -0500

    diversos cambios para la compatibilidad con celery

--- backendTest/app_delivery/complements/bot.py
+++ backendTest/app_delivery/complements/bot.py
@@ -1,18 +1,29 @@
 import slack
-import os
-from pathlib import Path
-from dotenv import load_dotenv
 
-class Bot_Slack:
+from celery import shared_task
+
+# class Bot_Slack:
+   
+#     def __init__(self, text):
+#         self.client = slack.WebClient(token="xoxb-974816393074-1989003701255-FuPS0TNCcmyA84or8NMkgMWV")
+#         # self.CHANNEL = "#backen-test-cornershop"
+#         self.channel = "#test"
+#         self.text=text
     
-    def __init__(self, text):
-        self.client = slack.WebClient(token="xoxb-974816393074-1989003701255-azhrmau374nDvHaZ3lkeZzOj")
-        # self.channel = "#backen-test-cornershop"
-        self.channel = "#test"
-        self.text=text
+#     @shared_task(self)
+#     def send_message(self):
+#         try:
+#             self.client.chat_postMessage(channel=self.channel, text=self.text)
+#         except:
+#             print("Message can't be send it")
 
-    def send_message(self):
-        try:
-            self.client.chat_postMessage(channel=self.channel, text=self.text)
-        except:
-            print("Message can't be send it")
+CLIENT = slack.WebClient(token="")
+#CHANNEL = "#backen-test-cornershop"
+CHANNEL = "#test"
+
+@shared_task
+def send_message(text):
+    try:
+        CLIENT.chat_postMessage(channel=CHANNEL, text=text)
+    except:
+        print("Message can't be send it")
